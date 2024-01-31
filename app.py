40f5489d185737820035511c0a450a16dc0d168d
Mahan Madani <93105700+Mahan-M47@users.noreply.github.com>
2024-01-31 19:37:34 +0330

    debug app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSequenceClassification
 
 
 id2label = {0: "Negative", 1: "Positive"}
 
 def classify(text):
-    inputs = tokenizer.encode(text, return_tensors="pt")
-    logits = model(inputs).logits
+    inputs = classification_tokenizer.encode(text, return_tensors="pt")
+    logits = classification_model(inputs).logits
     predictions = torch.argmax(logits)
 
     return id2label[predictions.tolist()]
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained("./model/v3")
     tokenizer = AutoTokenizer.from_pretrained("./model/v3")
 
-    classification_model = AutoModelForCausalLM.from_pretrained("./model/classification_v2")
+    classification_model = AutoModelForSequenceClassification.from_pretrained("./model/classification_v2")
     classification_tokenizer = AutoTokenizer.from_pretrained("./model/classification_v2")
 
     st.title("Steam Review Generator for Baldur's Gate 3")
@@ -27,12 +27,17 @@ if __name__ == "__main__":
 
     if st.button("Generate"):
         # Tokenize and generate text
-        input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        generated_text = model.generate(input_ids, do_sample=True, top_k=50, top_p=0.95, pad_token_id=tokenizer.pad_token_id, max_new_tokens=200)
+        if prompt == "":
+            generated_text = model.generate(do_sample=True, top_k=50, top_p=0.95, pad_token_id=tokenizer.pad_token_id, max_new_tokens=100)
+        else:
+            input_ids = tokenizer.encode(prompt, return_tensors="pt")
+            generated_text = model.generate(input_ids, do_sample=True, top_k=50, top_p=0.95, pad_token_id=tokenizer.pad_token_id, max_new_tokens=100)
+        
+        generated_text = tokenizer.decode(generated_text[0], skip_special_tokens=True)
 
         # Decode and display the generated text
         st.subheader("Generated Text:")
-        st.write(tokenizer.decode(generated_text[0], skip_special_tokens=True))
+        st.write(generated_text)
 
         # Display the review's polarity
         st.subheader("Review Type:")
